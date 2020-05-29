f5494bae477201371349864a0aaccb3aae7b6a5e
iklone <jamesthackway@hotmail.co.uk>
2020-05-29 16:04:34 +0100

    aligned grid margin

--- main.py
+++ main.py
@@ -3,36 +3,31 @@ import sys
 import time
 from PIL import Image
 
-def main(argv):
+def main(width, brightness):
     imgList = []
     n = 0
 
-    width = int(argv)
-    sWidth = width // 3
-    margin = 33
+    margin = width // 100
+    sWidth = (width - (4 * margin)) // 3
+    uWidth = sWidth + margin
 
     for file in os.listdir("."):
         if file != '3x3.png' and (file.endswith(".png") or file.endswith(".jpg")):
-            print(os.path.join(file))
             imgList.append(file)
             n += 1
             if n == 9:
                 break
     
-    img = Image.new('RGB', (width, width), (0, 0, 0))
+    img = Image.new('RGB', (width, width), (brightness, brightness, brightness))
 
     x = 0
     y = 0
     for file in imgList:
-        img.save('3x3.png')
-        time.sleep(0.2)
-        #sys.exit()
-
         image = Image.open(file, 'r')
-        image = image.resize((sWidth - margin, sWidth - margin))
-        img.paste(image, (x * sWidth, y * sWidth, (x * sWidth) + sWidth - margin, (y * sWidth) + sWidth - margin))
+        image = image.resize((sWidth, sWidth))
+        img.paste(image, ((x * uWidth) + margin, (y * uWidth) + margin, (x + 1) * uWidth, (y + 1) * uWidth))
 
-        print ("inserted" + file)
+        print (file)
         
         x += 1
         if x == 3:
@@ -43,4 +38,4 @@ def main(argv):
     img.save('3x3.png')
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(int(sys.argv[1]), int(sys.argv[2]))
